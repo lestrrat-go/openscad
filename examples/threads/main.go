c29598c4a6aa02ec1b28fd98180c21fc3ae08084
Daisuke Maki <lestrrat+github@gmail.com>
2023-06-23 07:47:03 +0900

    Add main.go

---
+++ examples/threads/main.go
@@ -0,0 +1,24 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/lestrrat-go/openscad/ast"
+	_ "github.com/lestrrat-go/openscad/examples/threads"
+)
+
+// Executing this as `go run main.go` will output the amalgamated OpenSCAD
+// code to generate the design.
+func main() {
+	if err := _main(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func _main() error {
+	return ast.EmitFile(`threads.scad`, os.Stdout, ast.WithAmalgamation())
+}
